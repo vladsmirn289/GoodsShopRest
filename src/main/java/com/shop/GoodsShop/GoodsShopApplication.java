880ae91d8f33eb9aac367c36af70993613538eff
Vladislav <raintype333@gmail.com>
2020-06-12 18:23:38 +1100

    Init project and base settings

---
+++ src/main/java/com/shop/GoodsShop/GoodsShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.GoodsShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/GoodsShop/GoodsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.GoodsShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
